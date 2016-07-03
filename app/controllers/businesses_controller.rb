34098906be5da21a8d42f6a748298736443fd02e
Gilles Crofils <gilles@secondbureau.com>
2016-07-03 12:12:09 +0800

    #2 improved menu

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -5,7 +5,7 @@ class BusinessesController < CrudController
   
   def add_menu_items
     @menu.update do |menu|
-      menu.update I18n.t("menu.businesses"), businesses_path, 'business', 'businesses', {icon: Business.decorator_class.icon} do |submenu|
+      menu.update I18n.t("menu.businesses"), businesses_path, 'businesses', {icon: Business.decorator_class.icon} do |submenu|
         submenu.add I18n.t("menu.business.all"), businesses_path, {icon: false}
         submenu.add I18n.t("menu.business.add_new"), new_business_path, {icon: false}
       end
